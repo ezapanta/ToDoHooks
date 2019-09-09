c4e11740d3be508fbf56dbdac8255b197befe322
Edward <zapanta.ed@gmail.com>
2019-09-08 19:00:15 -0500

    basic add/remove with undef verification

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  // Intitilize state variables
+  const [todos, setTodos] = useState([]);
+  const [pendingTodo, changeText] = useState('');
+
+  const verifyForm = (event) => {
+    event.preventDefault();
+    if(pendingTodo.length > 0) {
+      setTodos([...todos, { text: pendingTodo, id: todos.length + 1 }]); 
+    } else {
+      alert('Please type a task to enter it to the list')
+    }
+  }
+
+  const removeTodo = (event) => {
+    event.preventDefault();
+    setTodos(todos.filter(todo => todo.id === event.target.value))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" value={pendingTodo} onChange={(text) => changeText(text.target.value)}></input>
+        <button onClick={verifyForm}>Add</button>
+      </form>
+      <ul>
+        {todos.map(todo =>
+          <li key={todo.id} onClick={removeTodo}>{todo.text}</li>
+        )}
+      </ul>
     </div>
   );
 }
